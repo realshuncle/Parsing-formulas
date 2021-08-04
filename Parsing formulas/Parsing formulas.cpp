91ed5f04bef402f44ef33395c132a86c479ad8cd
Seraphim Tumachev <realshuncle@gmail.com>
2021-08-04 22:32:12 +0400

    Update Parsing formulas.cpp

--- Parsing formulas/Parsing formulas.cpp
+++ Parsing formulas/Parsing formulas.cpp
@@ -3,12 +3,6 @@
 
 using namespace std;
 
-//a function that checks if a character is a Latin letter
-//bool isLetter(char c) {
-//	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-//		return 1;
-//	return 0;
-//};
 //a function that checks if a character is a number
 bool isNumber(string str) 
 {
